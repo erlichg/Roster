0b7f9ca3d1ff0a5be5f77720d5e84dbe0f5310f1
Guy Erlich <erlichg@gmail.com>
2018-12-31 12:14:54 +0200

    change ldap

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const config = {
     mongoURL: process.env.MONGO_URL || `mongodb://localhost:27017/${db}`,
     // Authentication
     auth: {
-        ldapUrl: "ldap://app.corp.emc.com:3268",
-        baseDN: "dc=emc,dc=com"
+        ldapUrl: "ldap://10.146.130.182",
+        baseDN: "dc=corp,dc=emc,dc=com"
     }
 };
 
